0f7c87b1a388cea2b51f0c22f7932ea7dc5c0e72
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-16 20:10:37 +0600

    password Check done

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,20 @@
 // step-1 add click event handler with the submit button
 document.getElementById('btn-submit').addEventListener('click', function () {
-    
+    //step -2 get the email address indsde the email field
+    //always remember to use .value to get from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    //step -3 get password
+    //3.a = set id on the html elements
+    // 3.b = get the value
+    //3.c get the value from the element
+    const passwordField =document.getElementById('user-password');
+    const password = passwordField.value;
+    //step-4 verify email and password
+    if(email === 'rafi@gmail.com' && password ==='secret'){
+        console.log('Valid User')
+    }
+    else{
+        console.log('invalid User')
+    }
 })
